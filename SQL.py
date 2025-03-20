906699b3da8497d9f79df16ab07317325471e140
Milan2509 <167519193+Milan2509@users.noreply.github.com>
2025-03-20 16:44:47 +0100

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -1,23 +1,31 @@
 ### --------- Bibliotheken en globale variabelen ----------------
-import sqlite3
+import sqlite3 
 
 with sqlite3.connect("FilmDatabase.db") as db:
-    # cursor is object waarmee je data uit de database kan halen
+    # cursor is object waarmeWe je data uit de database kan halen
     cursor = db.cursor()
 
 ### --------- Functie definities -----------------
 def maakHoofdTabelAan():
-    # Maak een nieuwe tabel met 3 kolommen: id, naam, prijs ondertitelID INTEGER,
+    # Maak een nieuwe tabel met de naam 'tbl_films' als deze nog niet bestaat
+    # met de kolommen: filmID, titel, genre, studio, taal, lengte, trailer, ondertitels
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS tbl_films(
         filmID INTEGER PRIMARY KEY AUTOINCREMENT,
         titel TEXT NOT NULL UNIQUE,
-        genre TEXT NOT NULL,
         studio TEXT NOT NULL,
         taal TEXT NOT NULL,
+        genreID INTEGER,
         lengte INTEGER NOT NULL,
         trailer TEXT NOT NULL,
-        ondertitels JSON NOT NULL
+        ondertitels JSON NOT NULL,
+        FOREIGN KEY (genreID) REFERENCES tbl_genre_id(genreID)
+        );
+    """)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tbl_genre_id(
+        genreID INTEGER PRIMARY KEY AUTOINCREMENT,
+        genre TEXT NOT NULL
         );
     """)
     print("Tabel 'tbl_films' aangemaakt.")
@@ -33,20 +41,33 @@ def voegFilmToe(titel:str, genre:str, studio:str, taal:str, lengte:str, trailer:
     
     cursor.execute("SELECT titel FROM tbl_films")
     data = cursor.fetchall()
-
-    #for loop die checkt of de titel al bestaat in de database
-    db_heeft_titel = False
-    for i in range(len(data)):
-        if str(data[i]) == "('" + titel + "',)":
-            db_heeft_titel = True
-            continue #Gaat verder uit de for loop naar het volgende if statement: if(db_heeft_titel), zodat de for loop niet verder gaat met checken
     
-    # if statement die checkt of de titel al bestaat in de database
-    if(db_heeft_titel):
-        print("Kan ", titel, " Niet Toevoegen: Bestaat Al!")   
+    genre = genre.lower() # lowercase genre zodat de genre niet hoofdlettergevoelig is, en 2x dezelfde genre niet wordt toegevoegd
+
+    # checkt of de titel al bestaat in de database
+    db_heeft_titel = any(str(row) == f"('{titel}',)" for row in data)
+    
+    cursor.execute("SELECT genre FROM tbl_genre_id")
+
+    if db_heeft_titel:
+       print("Kan ", titel, " Niet Toevoegen: Bestaat Al!")   
     else:     
-        cursor.execute("INSERT INTO tbl_films VALUES(NULL, ?, ?, ?, ?, ?, ?, ?)", (titel, genre, studio, taal, lengte, trailer, '[' + ondertitels +']'))
-        db.commit()  # gegevens naar de database wegschrijven
+        # selcteer genreID van de genre die je wilt toevoegen
+        cursor.execute("SELECT genreID FROM tbl_genre_id WHERE genre = ?", (genre,))
+        genre_data = cursor.fetchone()
+        
+        # checkt of de genre id al bestaat, zo niet dan wordt deze toegevoegd
+        if genre_data is None:
+            cursor.execute("INSERT INTO tbl_genre_id (genre) VALUES (?)", (genre,))
+            db.commit()
+            cursor.execute("SELECT genreID FROM tbl_genre_id WHERE genre = ?", (genre,))
+            genreID = cursor.fetchone()[0]
+        else:
+            genreID = genre_data[0]
+        
+        # Insert the film into tbl_films with the retrieved genreID
+        cursor.execute("INSERT INTO tbl_films VALUES(NULL, ?, ?, ?, ?, ?, ?, ?)", (titel, studio, taal, genreID, lengte, trailer, '[' + ondertitels + ']'))
+        db.commit()
         print("Film ", titel, " Succesvol Toegevoegd")
 
 # in deze class worden alle functies gemaakt die data uit de database halen
@@ -63,7 +84,9 @@ class VraagFilmDataOp():
         return resultaat
     # vraagt alle genres op en returned deze
     def vraagFilmGenreOp(ID:int):
-        cursor.execute("SELECT genre FROM tbl_films WHERE filmID = ?", (ID))
+        cursor.execute("SELECT genreID FROM tbl_films WHERE filmID = ?", (ID))
+        genreID = cursor.fetchone()[0]
+        cursor.execute("SELECT genre FROM tbl_genre_id WHERE genreID = ?", (genreID,))
         resultaat = cursor.fetchall()
         return resultaat
     # vraagt alle studios op en returned deze
@@ -95,7 +118,7 @@ class VraagFilmDataOp():
 # voegt de films toe aan de database
 def voegAlleFilmsToe():
     cursor.execute("SELECT COUNT(*) FROM tbl_films") # telt het aantal films in de database
-    count = cursor.fetchone()[0] # zet het aantal films in de variabele count, fetchone haalt de eerste rij uit de query met de type tuple. De [0] zorgt er voor dat het eerste element uit de tuple word gehaald. Dit is nodig omdat Python bijna niks kan met tuples.
+    count = cursor.fetchone()[0] # zet het aantal films in de variabele count
     
     # als er nog geen films in de database staan, worden de films toegevoegd
     if count == 0:    
@@ -106,6 +129,7 @@ def voegAlleFilmsToe():
     # als er al films in de database staan, wordt er een melding gegeven
     else:
         print("Voorbeeld films worden niet toegevoegd, omdat er al films in de database staan.")
-### --------- Hoofdprogramma -----------------
+
+# main functies
 maakHoofdTabelAan()
 voegAlleFilmsToe()
